2a13e220ae9a34b66a16158597650f4ebb94e414
milanzagorac <zagoracm@gmail.com>
2021-05-17 08:56:48 -0400

    changes

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, flask, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db
 
+app = Flask(__name__)
