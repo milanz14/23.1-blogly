be4737524bbae3dd8c49228014d9694dfd9b8348
milanzagorac <zagoracm@gmail.com>
2021-05-17 09:20:59 -0400

    initialize flask app and add User model

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template, redirect, flask, session
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db
+from models import db, connect_db, User
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///bloglydb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =  False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'development_key'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
--- models.py
+++ models.py
@@ -1,3 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 
+# initialize SQLA and set up the server
 db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+# Model for User
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, default='https://images.unsplash.com/photo-1586314265219-192da32be7eb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80')
+
