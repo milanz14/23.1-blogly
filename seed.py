4ad56e5bf6db10d73a776eecf927111b9374da34
milanzagorac <zagoracm@gmail.com>
2021-05-18 19:48:15 -0400

    added some posts routes, new post form, updated models and updated seed file

--- seed.py
+++ seed.py
@@ -1,5 +1,6 @@
-from models import User, db
+from models import User, Post, db
 from app import app
+from datetime import datetime
 
 #create all tables and seed the data
 db.drop_all()
@@ -7,7 +8,11 @@ db.create_all()
 
 u1 = User(first_name='Ron', last_name='Swanson')
 u2 = User(first_name='Tom', last_name='Haverford')
+p1 = Post(title='First!', content='This is my first post!', created_at=datetime.now(), user_id=1)
+p2 = Post(title='Second...!', content='My second post! Neato!', created_at=datetime.now(), user_id=1)
 
 db.session.add(u1)
 db.session.add(u2)
+db.session.add(p1)
+db.session.add(p2)
 db.session.commit()
