d1c898e7627b3eef0df491c8983877e3be8fe162
milanzagorac <zagoracm@gmail.com>
2021-05-19 07:42:26 -0400

    post tests not working - fix

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from app import app
-from models import db, User
+from models import db, User, Post
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///bloglydb_test'
 app.config['SQLALCHEMY_ECHO'] = False
@@ -50,3 +50,41 @@ class UserModelTestCase(TestCase):
             resp = client.get(f'users/{self.user_id}/delete')
             html = resp.get_data(as_text=True)
             self.assertNotIn('Test User', html)
+
+class PostModelTestCase(TestCase):
+    """ Tests for Post model and any routes that have to do with user posts """
+    def setUp(self):
+        """ clean up any existing posts """
+        Post.query.delete()
+        test_post = Post(title='Test', content='Test Post Content')
+        db.session.add(test_post)
+        db.session.commit()
+        self.post = test_post
+        self.post_id = test_post.id
+        
+    def tearDown(self):
+        """ clean up fouled transactions """
+        db.session.rollback()
+
+    def test_show_users_posts(self):
+        """ test the posts seen on a user's page -
+        need to create a test user in the setUp method """
+        with app.test_client() as client:
+            pass
+
+    def test_posts_details(self):
+        """ test post details page """
+        with app.test_client() as client:
+            resp = client.get(f'/posts/{self.post_id}')
+            html = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<h3>Test</h3>', html)
+    
+    def test_edit_post(self):
+        """ test the edit post page """
+        with app.test_client() as client:
+            d = {"title": "Test","content":"this is the content"}
+            resp = client.post(f'/users/{self.post_id}/edit', data=d, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<h3>Test</h3>', html)
