0d0a3fd5f6e25129b295a51253eb248f271e9fa9
milanzagorac <zagoracm@gmail.com>
2021-05-24 11:29:49 -0400

    worked on tags but part 3 of assignment not fully functional

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 # initialize SQLA and set up the server
 db = SQLAlchemy()
@@ -28,17 +29,17 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.Text, nullable=False)
     content = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'),nullable=False)
     
 class PostTag(db.Model):
     __tablename__ = 'posttags'
-    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True, nullable=False)
-    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
 
 class Tag(db.Model):
     __tablename__ = 'tags'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     tag_name = db.Column(db.Text, unique=True, nullable=False)
-
+    posts = db.relationship('Post', secondary='posttags', backref='tags')
